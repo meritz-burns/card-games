17f53ca9a086779d83ab42302ca22e32ea69a546
Mike Burns <mike@mike-burns.com>
2020-09-26 04:40:48 -0400

    Limit the drop targets
    This is a complete re-write of the drag-and-drop functionality. Instead
    of connecting every droppable and using a 500 to indicate that it wasn't
    right, we can instead designate specific piles as droppable and clear
    this after each drag end.
    
    Add some CSS to make it more clear. On drag start we highlight the valid
    piles in green. On drag over give it a grey background just so you can
    see that you've done something. The whole while, make the original card
    half transparent.

--- app/javascript/packs/game.js
+++ app/javascript/packs/game.js
@@ -1,44 +1,122 @@
-function dragStartHandler(ev) {
-  const radio = ev.target.getElementsByTagName("input")[0];
+/* draggables */
 
-  ev.dataTransfer.setData("application/x-source-id", radio.id);
+function dragStart(ev) {
+  ev.target.classList.add("drag-target");
+  ev.dataTransfer.setData("application/x-source-id", ev.target.id);
   ev.dataTransfer.dropEffect = "move";
+
+  const source = findTarget(ev.target);
+  const dests = destsForSource(source);
+
+  for (dest of dests) {
+    dest.classList.add("drop-target");
+    dest.addEventListener("dragover", dragOver);
+    dest.addEventListener("dragenter", dragEnter);
+    dest.addEventListener("dragleave", dragLeave);
+    dest.addEventListener("drop", drop);
+  }
 }
 
-function dragoverHandler(ev) {
-  ev.preventDefault();
-  ev.dataTransfer.dropEffect = "move"
-}
+function dragEnd(ev) {
+  const source = findTarget(ev.target);
+  const dests = destsForSource(source);
 
-function dropHandler(ev) {
-  ev.preventDefault();
-
-  if (ev.dataTransfer === null)
-    return;
-
-  let dropSection = ev.target;
-  while (
-      dropSection !== null &&
-      dropSection.tagName !== "SECTION" &&
-      dropSection.tagName !== "BODY"
-  ) {
-    dropSection = dropSection.parentNode;
+  for (dest of dests) {
+    dest.classList.remove("drop-target");
+    dest.classList.remove("drop-over");
+    dest.removeEventListener("drop", drop);
+    dest.removeEventListener("dragleave", dragLeave);
+    dest.removeEventListener("dragenter", dragEnter);
+    dest.removeEventListener("dragover", dragOver);
   }
 
-  if (dropSection.tagName !== "SECTION")
+  ev.target.classList.remove("drag-target");
+}
+
+/* droppables */
+
+function dragOver(ev) {
+  ev.preventDefault();
+}
+
+function dragEnter(ev) {
+  const dest = findTarget(ev.target);
+
+  if (dest !== null)
+    dest.classList.add("drop-over");
+}
+
+function dragLeave(ev) {
+  const destWas = findTarget(ev.target);
+  const destIs = findTarget(ev.relatedTarget);
+
+  if (destWas !== null && destWas !== destIs)
+    destWas.classList.remove("drop-over");
+}
+
+function drop(ev) {
+  ev.preventDefault();
+
+  const dest = findTarget(ev.target);
+  if (dest === null)
     return;
 
-  const sourceRadioId = ev.dataTransfer.getData("application/x-source-id");
+  const data = ev.dataTransfer.getData("application/x-source-id");
+  const source = document.getElementById(data);
 
-  // get the form elements
-  const dropRadio = dropSection.getElementsByClassName("pile-radio")[0];
-  const sourceRadio = document.getElementById(sourceRadioId);
-  const form = document.getElementById("new_card_movement");
+  for (e of [dest, source]) {
+    e.getElementsByTagName("input")[0].checked = true;
+  }
 
-  // set up and submit the form
-  dropRadio.checked = true;
-  sourceRadio.checked = true;
-  form.submit();
+  document.getElementById("new_card_movement").submit();
+}
+
+/* helpers */
+
+function destsForSource(source) {
+  let result = [];
+
+  if (source.classList.contains("pile__hand")) {
+    result = [
+      ...document.getElementsByClassName("pile__board"),
+      document.getElementById("pile-deck"),
+      document.getElementById("pile-discard")
+    ];
+  } else if (source.classList.contains("pile__board")) {
+    result = [
+      ...document.getElementsByClassName("pile__hand"),
+      ...document.getElementsByClassName("pile__board"),
+      document.getElementById("pile-discard")
+    ];
+  } else if (source.classList.contains("pile__deck")) {
+    result = [...document.getElementsByClassName("pile__hand")];
+  } else if (source.classList.contains("pile__discard")) {
+    result = [...document.getElementsByClassName("pile__hand")];
+  } else {
+    result = [];
+  }
+
+  return result.filter(e => e !== source);
+}
+
+function findTarget(t) {
+  while (
+      t !== null &&
+      t.tagName !== "SECTION" &&
+      t.tagName !== "BODY"
+  ) {
+    t = t.parentNode;
+  }
+
+  if (t !== null && t.tagName === "SECTION")
+    return t;
+  else
+    return null;
+}
+
+function connectDragables() {
+  document.addEventListener("dragstart", dragStart);
+  document.addEventListener("dragend", dragEnd);
 }
 
 function hideFormWidgets() {
@@ -49,26 +127,9 @@ function hideFormWidgets() {
   }
 }
 
+/* main */
+
 window.addEventListener('DOMContentLoaded', () => {
-  const cards = document.getElementsByClassName("card");
-  const maskedCards = document.getElementsByClassName("masked-card");
-  const piles = document.getElementsByClassName("pile");
-
-  for (element of cards) {
-    element.addEventListener("dragstart", dragStartHandler);
-  }
-
-  for (element of maskedCards) {
-    element.addEventListener("dragstart", dragStartHandler);
-  }
-
-  document.getElementById("pile-deck-top")
-    .addEventListener("dragstart", dragStartHandler);
-
-  for (element of piles) {
-    element.addEventListener("drop", dropHandler);
-    element.addEventListener("dragover", dragoverHandler);
-  }
-
+  connectDragables();
   hideFormWidgets();
 });
