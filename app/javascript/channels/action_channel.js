509a30d361018e323fd2851b5f499cc479adb063
Mike Burns <mike@mike-burns.com>
2020-09-29 01:32:09 -0400

    Show a masked card for the deck
    Change the way we render both the deck and the discard pile to be a
    "card count": show a pseudo card with a size, then offset a card atop
    it. This has a nice effect where you can see the top card (masked, for
    the deck) and then below that is a second card-like thing peaking out
    with the number of cards in the pile.
    
    We want to handle the case where the discard or deck are empty. We do
    this by grabbing any existing card, removing it if it exists, then
    adding our new card.
    
    Along the way I learned some DOM:
    
    - [`ChildNode.remove()`] is the new `c.parentElement.removeChild(c)`.
    - [`HTMLCollection`] can index by element `name` in addition to treating
    it like an array. Turns out this is how
    `document.forms["card_movement"]` works.
    
    [`ChildNode.remove()`]: https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/remove
    [`HTMLCollection`]: https://developer.mozilla.org/en-US/docs/Web/API/HTMLCollection

--- app/javascript/channels/action_channel.js
+++ app/javascript/channels/action_channel.js
@@ -1,37 +1,26 @@
 import consumer from "./consumer"
 
-function addToDeck(cardId) {
-  let deckPile = document.getElementById("pile-deck-top");
+function setDeck(cardId, delta) {
+  let deckPile = document.getElementById("pile-deck");
   let deckSize = document.getElementById("pile-deck-size");
   let deckInt = parseInt(deckSize.textContent);
-  let radio = deckPile.getElementsByTagName("input")[0];
-  let label = deckPile.getElementsByTagName("label")[0];
+  let existingCard = deckPile.getElementsByClassName("masked-card")[0];
+  let cards = deckPile.getElementsByClassName("cards")[0];
 
   if (deckInt === NaN) {
     deckSize.textContent = 1;
   } else {
-    deckSize.textContent = deckInt + 1;
+    deckSize.textContent = deckInt + delta;
   }
 
-  radio.setAttribute("id", "card_movement_source_" + cardId + "-in-pile-deck");
-  radio.setAttribute("value", cardId + "-in-pile-deck");
-  label.setAttribute("for", radio.getAttribute("id"));
-}
-
-function removeFromDeck(newTopCardId) {
-  let deckPile = document.getElementById("pile-deck-top");
-  let deckSize = document.getElementById("pile-deck-size");
-  let deckInt = parseInt(deckSize.textContent);
-  let radio = deckPile.getElementsByTagName("input")[0];
-  let label = deckPile.getElementsByTagName("label")[0];
-
-  if (deckInt != NaN && deckInt > 0) {
-    deckSize.textContent = (deckInt - 1);
+  if (existingCard !== undefined) {
+    existingCard.remove();
   }
 
-  radio.setAttribute("id", "card_movement_source_" + newTopCardId + "-in-pile-deck");
-  radio.setAttribute("value", newTopCardId + "-in-pile-deck");
-  label.setAttribute("for", radio.getAttribute("id"));
+  if (cardId !== null) {
+    let newTopCard = buildMaskedCardElement(cardId, "pile-deck");
+    cards.appendChild(newTopCard);
+  }
 }
 
 function addMaskedCard(cardId, pile) {
@@ -45,7 +34,7 @@ function addMaskedCard(cardId, pile) {
 function removeMaskedCard(cardId) {
   let cardElement = document.getElementById(cardId);
 
-  cardElement.parentElement.removeChild(cardElement);
+  cardElement.remove();
 }
 
 function addCard(card, pile) {
@@ -69,19 +58,26 @@ function addPotentiallyMaskedCard(card, cardId, pile) {
     addMaskedCard(cardId, pile);
 }
 
-function setDiscard(card) {
+function setDiscard(card, delta) {
   let discardPile = document.getElementById("pile-discard");
+  let discardSize = document.getElementById("pile-discard-size");
+  let discardInt = parseInt(discardSize.textContent);
+  let existingCard = discardPile.getElementsByClassName("card")[0];
   let cards = discardPile.getElementsByClassName("cards")[0];
 
-  if (card == null) {
-    debugger;
+  if (discardInt === NaN) {
+    discardSize.textContent = 1;
   } else {
-    let cardHtml = buildCardElement(card, "pile-discard");
+    discardSize.textContent = discardInt + delta;
+  }
 
-    while (cards.firstChild) {
-      cards.removeChild(cards.firstChild);
-    }
-    cards.appendChild(cardHtml);
+  if (existingCard !== undefined) {
+    existingCard.remove();
+  }
+
+  if (card !== null) {
+    let newTopCard = buildCardElement(card, "pile-discard");
+    cards.appendChild(newTopCard);
   }
 }
 
@@ -91,7 +87,7 @@ function buildMaskedCardElement(cardId, pile) {
 
   newCard.setAttribute("id", cardId);
 
-  const radio = newCard.getElementsByTagName("input")[0];
+  const radio = newCard.getElementsByTagName("input")["card_movement[source]"];
   radio.setAttribute("id","card_movement_source_" + cardId + "-in-" + pile);
   radio.setAttribute("value", cardId + "-in-" + pile);
   radio.hidden = true;
@@ -110,7 +106,7 @@ function buildCardElement(card, pile) {
   newCard.setAttribute("class", "card card__" + card.type);
   newCard.setAttribute("id", card.id);
 
-  const radio = newCard.getElementsByTagName("input")[0];
+  const radio = newCard.getElementsByTagName("input")["card_movement[source]"];
   radio.setAttribute("id","card_movement_source_" + card.id + "-in-" + pile);
   radio.setAttribute("value", card.id + "-in-" + pile);
   radio.hidden = true;
@@ -131,6 +127,12 @@ function buildCardElement(card, pile) {
   cardAbility.innerHTML = card.ability;
 
   return newCard;
+}
+
+function matchingLabel(container, input) {
+  return Array.
+    from(container.getElementsByTagName("label")).
+    find(function(l) { return l.getAttribute("for") === input.id });
 }
 
 consumer.subscriptions.create("ActionChannel", {
@@ -155,14 +157,14 @@ consumer.subscriptions.create("ActionChannel", {
         break;
       case "discard":
         removeCard(data["source_card"].id, data["source_pile"]);
-        setDiscard(data["source_card"]);
+        setDiscard(data["source_card"], +1);
         break;
       }
       break;
     case "deck":
       switch (data["dest_pile_type"]) {
       case "hand":
-        removeFromDeck(data["extra"]["new_top_card_id"]);
+        setDeck(data["extra"]["new_top_card_id"], -1)
         addPotentiallyMaskedCard(
             data["source_card"],
             data["extra"]["source_card_id"],
@@ -174,7 +176,7 @@ consumer.subscriptions.create("ActionChannel", {
     case "discard":
       switch (data["dest_pile_type"]) {
       case "hand":
-        setDiscard(data["extra"]["discard"]);
+        setDiscard(data["extra"]["discard"], -1);
         addPotentiallyMaskedCard(
             data["source_card"],
             data["extra"]["source_card_id"],
@@ -191,11 +193,11 @@ consumer.subscriptions.create("ActionChannel", {
         break;
       case "deck": // pile-deck, pile-deck-bottom
         removeMaskedCard(data["extra"]["source_card_id"]);
-        addToDeck(data["extra"]["new_top_card_id"]);
+        setDeck(data["extra"]["new_top_card_id"], +1);
         break;
       case "discard":
         removeMaskedCard(data["extra"]["source_card_id"]);
-        setDiscard(data["extra"]["discard"]);
+        setDiscard(data["extra"]["discard"], +1);
         break;
       }
     }
